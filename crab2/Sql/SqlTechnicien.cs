276773c511e31806673944801a6bb638ff8249bb
tlg <thierry28220@icloud.com>
2023-10-13 16:43:56 +0200

    ok

---
+++ crab2/Sql/SqlTechnicien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+
+namespace crab2.Sql
+{
+    internal class SqlTechnicien
+    {
+    }
+}
